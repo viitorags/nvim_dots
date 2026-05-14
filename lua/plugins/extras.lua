c0454c227f48e8d6bbf7386f3e83fe748cc7f7c3
vytorags <152658654+vytorags@users.noreply.github.com>
2026-05-14 16:30:58 -0300

    feat: add build hook to automatically install markdown-preview.nvim dependencies

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -18,6 +18,9 @@ return {
 	},
 	{
 		"iamcco/markdown-preview.nvim",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
